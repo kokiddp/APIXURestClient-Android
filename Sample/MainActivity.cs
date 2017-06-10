9d340e0ae3a6338711201f386752f7384e6e4651
Gabriele Coquillard <gabriele.digival@gmail.com>
2017-06-10 02:31:43 +0200

    Added sample

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using APIXURestClient;
+
+namespace APIXURestClientSample
+{
+    [Activity(Label = "Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        APIXURestApi MyAPIXUClient = new APIXURestApi("YOUR API KEY");
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            APIXUCurrentWeather MyCurrenWeather = MyAPIXUClient.GetCurrentWeather("aosta");
+            APIXUCurrentWeather MyCurrentWeatherLocalized = MyAPIXUClient.GetCurrentWeatherLocalized("aosta", APIXUL10n.Italian);
+            APIXUForecastWeather MyForecastWeather = MyAPIXUClient.GetForecastWeatherLocalized("aosta", 7, APIXUL10n.Italian);
+            APIXUForecastWeather MyForecastWeatherLocalized = MyAPIXUClient.GetForecastWeatherLocalized("aosta", 7, APIXUL10n.Italian);
+
+        }
+    }
+}
+
