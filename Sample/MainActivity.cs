02d3b142b9b0e11708822d792f661f94ff7cfd23
Gabriele Coquillard <gabriele.digival@gmail.com>
2017-06-10 02:51:15 +0200

    Updated sample with async methods

--- Sample/MainActivity.cs
+++ Sample/MainActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Android.App;
 using Android.OS;
+
+using System.Threading.Tasks;
+
 using APIXURestClient;
 
 namespace APIXURestClientSample
@@ -7,18 +10,33 @@ namespace APIXURestClientSample
     [Activity(Label = "Sample", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-
+        //Get an instance of the client by passing your API Key to the constructor
         APIXURestApi MyAPIXUClient = new APIXURestApi("YOUR API KEY");
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            APIXUCurrentWeather MyCurrenWeather = MyAPIXUClient.GetCurrentWeather("aosta");
-            APIXUCurrentWeather MyCurrentWeatherLocalized = MyAPIXUClient.GetCurrentWeatherLocalized("aosta", APIXUL10n.Italian);
-            APIXUForecastWeather MyForecastWeather = MyAPIXUClient.GetForecastWeatherLocalized("aosta", 7, APIXUL10n.Italian);
-            APIXUForecastWeather MyForecastWeatherLocalized = MyAPIXUClient.GetForecastWeatherLocalized("aosta", 7, APIXUL10n.Italian);
+            Task.Run(() => GetWeatherAsync());
 
+            //Get weather by town synchronously
+            //Current
+            APIXUCurrentWeather MyCurrenWeather = MyAPIXUClient.GetCurrentWeather("aosta");
+            //Current localized
+            APIXUCurrentWeather MyCurrentWeatherLocalized = MyAPIXUClient.GetCurrentWeatherLocalized("aosta", APIXUL10n.Italian);
+            //Forecast
+            APIXUForecastWeather MyForecastWeather = MyAPIXUClient.GetForecastWeather("aosta", 7);
+            //Forecast localized
+            APIXUForecastWeather MyForecastWeatherLocalized = MyAPIXUClient.GetForecastWeatherLocalized("aosta", 7, APIXUL10n.Italian);
+        }
+
+        //Get weather by town asynchronously
+        public async void GetWeatherAsync()
+        {
+            APIXUCurrentWeather MyCurrenWeatherAsync = await MyAPIXUClient.GetCurrentWeatherAsync("aosta");
+            APIXUCurrentWeather MyCurrenWeatherLocalizedAsync = await MyAPIXUClient.GetCurrentWeatherLocalizedAsync("aosta", APIXUL10n.Italian);
+            APIXUForecastWeather MyForecastWeatherAsync = await MyAPIXUClient.GetForecastWeatherAsync("aosta", 7);
+            APIXUForecastWeather MyForecastWeatherLocalizedAsync = await MyAPIXUClient.GetForecastWeatherLocalizedAsync("aosta", 7, APIXUL10n.Italian);
         }
     }
 }
